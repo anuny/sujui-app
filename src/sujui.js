5aa1ffcaa8644cb39ac58cbe6066ddb64aa32484
YangFei <yangfei88@live.cn>
2016-04-29 16:45:24 +0800

    add exports

--- src/sujui.js
+++ src/sujui.js
@@ -1,33 +1,49 @@
 (function(w, undefined) {
-	var app = new Object;
+	var app ={},
+	cache = {},
+	type = function(val) {return null == val ? val + "": Array == val.constructor ? "array": typeof val},
+	isAarray = function(val) {return type(val) === 'array'},
+	isBoolean = function(val) {return type(val) === 'boolean'},
+	isObject = function(val) {return type(val) === 'object'},
+	isFunction = function(val) {return type(val) === 'function'},
+	isEmptyObject = function(val) {for (var val in val) return false;return true};
 	app.version = '0.0.1';
 	app.modules = {};
 	app.extend = function(module) {
 		var i = 0,target = this,deep = false,length = arguments.length,obj, empty, items, x;
-		typeof arguments[0] === 'boolean' ? (deep = true, i = 1, length > 2 ? (i = 2, target = arguments[1]) : void 0) : (length > 1 ? (i = 1, target = arguments[0]) : void 0);
+		isBoolean(arguments[0]) ? (deep = true, i = 1, length > 2 ? (i = 2, target = arguments[1]) : void 0) : (length > 1 ? (i = 1, target = arguments[0]) : void 0);
 		for (x = i; x < length; x++) {
 			obj = arguments[x];
 			for (items in obj) {
 				if (obj[items] === target) continue;
-				deep && typeof obj[items] === 'object' && obj[items] !== null ? (empty = Array === obj[items].constructor ? [] : {},
+				deep && isObject(obj[items]) && obj[items] !== null ? (empty = isAarray(obj[items]) ? [] : {},
 				target[items] = app.extend(deep, target[items] || empty, obj[items])) : target[items] = obj[items]
 			}
 		};
 		return target
 	};
 	app.extend({
-		define : function(id, fn ,type) {
-			var err="module:[" + id + "] already defined";
-			return app.modules[id]=app.modules[id]?(type?fn(app.require):((w.console?console.warn(err):alert(err)),app.modules[id])):fn(app.require);
+		define: function(id, factory, cover) {
+			cache[id] = {};
+			var build = function() {
+				return isFunction(factory) ? factory(app.require, cache[id], module) : factory
+			},
+			module = {
+				id: id,
+				cover: cover ? cover: false,
+				type: type(factory)
+			},
+			err = "module:[" + id + "] already defined";
+			app.modules[id] = app.modules[id] ? (type ? build() : ((w.console ? console.warn(err) : alert(err)), app.modules[id])) : build()
 		},
-		require : function(id) {
+		require: function(id) {
 			return app.modules[id]
 		},
-		use : function(id, fn) {
-			var exports = [];
-			id = Array == id.constructor? id: [id];
-			for (var i = 0; i < id.length; i++) exports[i] = app.modules[id[i]];
-			fn && fn.apply(app, exports)
+		use: function(id, factory) {
+			var exports = [],cached = cache[id],i = 0,x,length = id.length;
+			id = isAarray(id) ? id: [id];
+			for (x = i; x < length; x++) exports[i] = isEmptyObject(cached) ? app.modules[id[i]] : [cached][0];
+			factory && factory.apply(app, exports)
 		}
 	});
 	w.app = w.APP = app
